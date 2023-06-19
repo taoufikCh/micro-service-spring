85ff1df6a98975075e41546fc228b96405dba746
taoufikCh <taoufikchenini@gmail.com>
2023-06-19 13:34:55 +0100

    Add files via upload

---
+++ security-gateway/src/main/java/com/auth/services/securitygateway/SecurityGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.auth.services.securitygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityGatewayApplication.class, args);
+    }
+
+}
---
+++ security-gateway/src/main/java/com/auth/services/securitygateway/entities/Role.java
@@ -0,0 +1,6 @@
+package com.auth.services.securitygateway.entities;
+
+public enum Role {
+    USER,
+    ADMIN
+}
